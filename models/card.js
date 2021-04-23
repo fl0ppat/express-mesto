8fe9c7fda1f3fc04dd1dd16cee293189cc3f9da0
Daniel Khrenov <klaven011@gmail.com>
2021-04-24 01:37:39 +0300

    fix: remove filed __v from documents

--- models/card.js
+++ models/card.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 
-const cardsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+const cardsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    ceatedAt: {
+      type: Date,
+      default: () => new Date(),
+    },
   },
-  link: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  likes: {
-    type: Array,
-    default: [],
-  },
-  ceatedAt: {
-    type: Date,
-    default: () => new Date(),
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("card", cardsSchema);
