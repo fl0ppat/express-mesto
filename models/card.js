d66b8c425bcdf26bbc347d3a15722478dde02c57
Daniel Khrenov <klaven011@gmail.com>
2021-04-24 01:06:10 +0300

    feat: add card model for mongoDB

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const cardsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+  ceatedAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+module.exports = mongoose.model("card", cardsSchema);
