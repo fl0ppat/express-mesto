7120318dd2633ae2da30d75e503c369722829c48
Daniel Khrenov <klaven011@gmail.com>
2021-04-24 01:06:56 +0300

    feat: add cards controllers

---
+++ controllers/cards.js
@@ -0,0 +1,64 @@
+const Card = require("../models/card");
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ cards }))
+    .catch((err) =>
+      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`)
+    );
+};
+
+module.exports.deleteCardById = (req, res) => {
+  console.log(req.params.cardId);
+  Card.findByIdAndRemove(req.params.cardId)
+    .then(() => {
+      Card.find({})
+        .then((cards) => {
+          res.send({ cards });
+        })
+        .catch((err) =>
+          res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`)
+        );
+    })
+    .catch((err) => {
+      res
+        .status(404)
+        .send({ message: `Карточка с id ${req.params.cardId} не найдена` });
+    });
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((user) => res.send({ data: user }))
+    .catch((err) =>
+      res
+        .status(400)
+        .send({ message: `Получены некорректные данные. ${err.message}` })
+    );
+};
+
+module.exports.setCardLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => res.send(card))
+    .catch((err) =>
+      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`)
+    );
+};
+
+module.exports.deleteCardLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => res.send(card))
+    .catch((err) =>
+      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`)
+    );
+};
