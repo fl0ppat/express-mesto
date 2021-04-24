80f1f5f264d12408640acac37f70864e28e2492e
Daniel Khrenov <klaven011@gmail.com>
2021-04-24 16:22:34 +0300

    fix: fix second review issues

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,38 +1,32 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
+
+function handleError(err, req, res) {
+  if (err.message === "NotFound") {
+    res.status(404).send({ message: `Пост с id ${req.params.id} не найден` });
+  } else if (err.name === "CastError") {
+    res.status(400).send({
+      message: `Получены некорректные данные`,
+    });
+  } else {
+    res
+      .status(500)
+      .send({ message: `Внутренняя ошибка сервера. ${err.message}` });
+  }
+}
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else {
-        res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-      }
-    });
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then(() => {
-      res.status(200).send({ message: 'Пост удалён' });
+      res.status(200).send({ message: "Пост удалён" });
     })
-    .orFail((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else if (err.name === 'DocumentNotFoundError') {
-        res
-          .status(404)
-          .send({ message: `Пользователь с id ${req.params.id} не найден` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .orFail(new Error("NotFound"))
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.createCard = (req, res) => {
@@ -40,59 +34,27 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else {
-        res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-      }
-    });
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.setCardLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
-    { new: true },
+    { new: true }
   )
+    .orFail(new Error("NotFound"))
     .then((card) => res.send(card))
-    .orFail((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else if (err.name === 'DocumentNotFoundError') {
-        res
-          .status(404)
-          .send({ message: `Пользователь с id ${req.params.id} не найден` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.deleteCardLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
-    { new: true },
+    { new: true }
   )
+    .orFail(new Error("NotFound"))
     .then((card) => res.send(card))
-    .orFail((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else if (err.name === 'DocumentNotFoundError') {
-        res
-          .status(404)
-          .send({ message: `Пользователь с id ${req.params.id} не найден` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .catch((err) => handleError(err, req, res));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,26 +1,20 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ users }))
-    .catch((err) => res
-      .status(500)
-      .send({ message: `Внутренняя ошибка сервера. ${err.message}` }));
+    .catch((err) =>
+      res
+        .status(500)
+        .send({ message: `Внутренняя ошибка сервера. ${err.message}` })
+    );
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
-      res.send(user);
-    })
-    .orFail(
-      res
-        .status(404)
-        .send({ message: `Пользователь с id ${req.params.id} не найден` }),
-    )
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .orFail(new Error("NotFound"))
+    .then((user) => res.send(user))
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.createUser = (req, res) => {
@@ -28,15 +22,7 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({
-          message: `Получены некорректные данные при создании пользователя. ${err.message}`,
-        });
-      } else {
-        res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-      }
-    });
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.updateUserData = (req, res) => {
@@ -52,42 +38,14 @@ module.exports.updateUserData = (req, res) => {
   }
 
   User.findByIdAndUpdate(req.user._id, newData)
-    .then((user) => {
-      res.send(user);
-    })
-    .orFail((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else if (err.name === 'DocumentNotFoundError') {
-        res
-          .status(404)
-          .send({ message: `Пользователь с id ${req.params.id} не найден` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .orFail(new Error("NotFound"))
+    .then((user) => res.send(user))
+    .catch((err) => handleError(err, req, res));
 };
 
 module.exports.updateUserAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar })
-    .then((user) => {
-      res.send(user);
-    })
-    .orFail((err) => {
-      if (err.name === 'CastError') {
-        res
-          .status(400)
-          .send({ message: `Получены некорректные данные. ${err.message}` });
-      } else if (err.name === 'DocumentNotFoundError') {
-        res
-          .status(404)
-          .send({ message: `Пользователь с id ${req.params.id} не найден` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send(`Внутренняя ошибка сервера. ${err.message}`);
-    });
+    .orFail(new Error("NotFound"))
+    .then((user) => res.send(user))
+    .catch((err) => handleError(err, req, res));
 };
