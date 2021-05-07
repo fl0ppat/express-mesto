0676303f251fb0622bb1acbaa9157d4b5eb80f39
Floppy Di <klaven011@gmail.com>
2021-05-07 14:40:03 +0300

    feat: add new error handler

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,43 +1,12 @@
-const handleError = (err, res) => {
-  switch (err.code) {
-    case 400:
-      res
-        .status(400)
-        .send({ message: `Request params error. ${err.message && err.message}` });
-      break;
-
-    case 401:
-      res
-        .status(401)
-        .send({ message: 'Wrong email or password.' });
-      break;
-
-    case 404:
-      res
-        .status(404)
-        .send({ message: `${err.message} not found.` });
-      break;
-
-    case 409:
-      res
-        .status(409)
-        .send({ message: 'Email is already taken.' });
-      break;
-
-    default:
-      res
-        .status(500)
-        .send({ message: 'Internal server error.' });
-      break;
-  }
+const handleError = (err, req, res, next) => {
+  const { code = 500, message } = err;
+  res
+    .status(code)
+    .send({
+      message: code === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 };
 
-class ErrorHandler extends Error {
-  constructor(code, message) {
-    super();
-    this.code = code;
-    this.message = message;
-  }
-}
-
-module.exports = { ErrorHandler, handleError };
+module.exports = handleError;
