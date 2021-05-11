b0eb54a9e8a498798ed5eae9ecf09b37bbcaa20d
Daniel Khrenov <klaven011@gmail.com>
2021-05-11 23:09:06 +0300

    fix: fix first review issues

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { isCelebrateError } = require('celebrate');
 const NotFoundError = require('./errors/NotFound');
 const handleError = require('./middlewares/errors');
+const BadRequestError = require('./errors/BadRequest');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,15 +18,23 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 
-app.post('/signin', require('./routers/users').login);
-app.post('/signup', require('./routers/users').createUser);
+app.use(require('./routers/auth'));
 
 app.use(require('./middlewares/auth'));
 
 app.use('/users', require('./routers/users'));
 app.use('/cards', require('./routers/cards'));
 
-app.use('/', (err, req, res, next) => next(new NotFoundError('Ресурс не найден')));
+app.use('/', (req, res, next) => next(new NotFoundError('Ресурс не найден')));
+
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV.trim() === 'dev') {
+    console.log(err);
+  }
+
+  if (isCelebrateError(err) || err.name === 'CastError') return next(new BadRequestError('Ошибка в полученных данных'));
+  return next(err);
+});
 
 app.use((err, req, res, next) => handleError(err, req, res, next));
 
