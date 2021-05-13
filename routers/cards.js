67df20566e467ff3e4855b71be2a9eeb6975a170
Daniel Khrenov <klaven011@gmail.com>
2021-05-13 10:40:35 +0300

    feat: add more error messages for all routes

--- routers/cards.js
+++ routers/cards.js
@@ -9,23 +9,30 @@ const {
   setCardLike,
   deleteCardLike,
 } = require('../controllers/cards');
-const BadRequestError = require('../errors/BadRequest');
 
 router.get('/', getCards);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24).message('Ошибка в полученном id'),
   }),
 }), deleteCardById);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().min(2).required()
+    name: Joi.string().min(2).max(30).required()
+      .messages({
+        'any.required': 'Название карточки обязательно',
+        'string.min': 'Название карточки должно содержать минимум {#limit} символа',
+        'sting.max': 'Название карточки должно содержать максимум {#limit} символа',
+      }),
+    link: Joi.string().required()
       .custom((value) => {
         if (validator.isURL(value, { require_protocol: true })) return value;
-        throw new BadRequestError();
+        throw Error();
+      }).messages({
+        'any.required': 'Ссылка на изображение обязательно',
+        'any.custom': 'Указана невалидная ссылка на изображение',
       }),
 
   }),
@@ -33,13 +40,13 @@ router.post('/', celebrate({
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24).message('Ошибка в полученном id'),
   }),
 }), setCardLike);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24).message('Ошибка в полученном id'),
   }),
 }), deleteCardLike);
 
--- routers/users.js
+++ routers/users.js
@@ -9,39 +9,31 @@ const {
   updateUserData,
   updateUserAvatar,
 } = require('../controllers/users');
-const BadRequestError = require('../errors/BadRequest');
 
 router.get('/', getUsers);
-
-router.get('/me', celebrate({
-  query: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
-  }),
-}), getAuthUser);
+router.get('/me', getAuthUser);
 
 router.get('/:id', celebrate({
   query: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
+    id: Joi.string().hex().length(24).message('Ошибка в полученном id'),
   }),
 }), getUserById);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().alphanum().min(2).max(30),
-    about: Joi.string().alphanum().min(2).max(30),
+    name: Joi.string().min(2).max(30).message('Имя должно содержать не менее 2 и не более 30 символов'),
+    about: Joi.string().min(2).max(30).message('Информация должна содержать не менее 2 и не более 30 символов'),
   }),
 }), updateUserData);
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().alphanum().min(2),
     avatar: Joi.string().min(2).required()
       .custom((value) => {
         if (validator.isURL(value, { require_protocol: true })) return value;
-        throw new BadRequestError();
+        throw new Error();
       })
-    ,
-
+      .message('Ссылка на аватар не прошла проверку'),
   }),
 }), updateUserAvatar);
 
